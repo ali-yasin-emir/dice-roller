d5cf3ab77576835d116a6fad39428a8e5929d197
WeaktoN <aliyasin.emir16@hotmail.com>
2021-10-08 01:29:24 +0300

    All files have been added.

--- app/src/main/java/com/example/dice_roll/MainActivity.kt
+++ app/src/main/java/com/example/dice_roll/MainActivity.kt
@@ -11,33 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var rollButton: Button = findViewById(R.id.roll)
-        var showNumber: TextView = findViewById(R.id.textView)
-        var showDice: ImageView = findViewById(R.id.imageView)
+        val rollButton: Button = findViewById(R.id.roll)
+        val showNumber: TextView = findViewById(R.id.textView)
+        val showDice: ImageView = findViewById(R.id.imageView)
 
         showDice.setImageResource(R.drawable.dice_1)
 
         rollButton.setOnClickListener {
 
-    var rastgele = (1..6).random()
-    showNumber.text = rastgele.toString()
+            val random = (1..6).random()
+            showNumber.text = random.toString()
 
-when(rastgele){
+            when (random) {
 
-    1-> showDice.setImageResource(R.drawable.dice_1)
-    2-> showDice.setImageResource(R.drawable.dice_2)
-    3-> showDice.setImageResource(R.drawable.dice_3)
-    4-> showDice.setImageResource(R.drawable.dice_4)
-    5-> showDice.setImageResource(R.drawable.dice_5)
-    6-> showDice.setImageResource(R.drawable.dice_6)
-
-}
-
-
-
-}
-
-
+                1 -> showDice.setImageResource(R.drawable.dice_1)
+                2 -> showDice.setImageResource(R.drawable.dice_2)
+                3 -> showDice.setImageResource(R.drawable.dice_3)
+                4 -> showDice.setImageResource(R.drawable.dice_4)
+                5 -> showDice.setImageResource(R.drawable.dice_5)
+                6 -> showDice.setImageResource(R.drawable.dice_6)
+            }
+        }
     }
 }
 
